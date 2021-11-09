23ec1b9711b6c831a18a9e47240f2cc907a13afc
Dmitry R <dimsog@gmail.com>
2021-11-09 15:58:26 +0300

    Add translate to categories

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -1,10 +1,33 @@
 <?php
 
 return [
+    'categories' => 'Categories',
+    'slides' => 'Slides',
     'plugin' => [
         'name' => 'Slider',
         'description' => 'Free swiper slider adaptation for wintercms'
     ],
-    'categories' => 'Categories',
-    'slides' => 'Slides'
+    'buttons' => [
+        'create' => 'Create',
+        'create_and_close' => 'Create and Close',
+        'save' => 'Save',
+        'save_and_close' => 'Save and close',
+        'cancel' => 'Cancel'
+    ],
+    'controllers' => [
+        'categories' => [
+            'new' => 'New Category',
+
+            'list' => 'Manage Categories',
+            'return_to_list' => 'Return to categories list',
+
+            'delete_request_confirm' => 'Are you sure you want to delete the selected Categories?',
+            'delete_selected' => 'Delete selected',
+            'deleting' => 'Deleting Category...',
+            'delete_category_confirm' => 'Delete this category?',
+
+            'creating' => 'Creating Category...',
+            'saving' => 'Saving Category...'
+        ]
+    ]
 ];
