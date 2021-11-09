cb4f17637e91e6e03627df73555099c91546bcb2
Dmitry R <dimsog@gmail.com>
2021-11-09 12:53:04 +0300

    Add default controllers

---
+++ controllers/Categories.php
@@ -0,0 +1,35 @@
+<?php namespace Dimsog\Slider\Controllers;
+
+use BackendMenu;
+use Backend\Classes\Controller;
+
+/**
+ * Categories Back-end Controller
+ */
+class Categories extends Controller
+{
+    /**
+     * @var array Behaviors that are implemented by this controller.
+     */
+    public $implement = [
+        'Backend.Behaviors.FormController',
+        'Backend.Behaviors.ListController'
+    ];
+
+    /**
+     * @var string Configuration file for the `FormController` behavior.
+     */
+    public $formConfig = 'config_form.yaml';
+
+    /**
+     * @var string Configuration file for the `ListController` behavior.
+     */
+    public $listConfig = 'config_list.yaml';
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        BackendMenu::setContext('Dimsog.Slider', 'slider', 'categories');
+    }
+}
