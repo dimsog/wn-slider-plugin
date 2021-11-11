88e7ad072473c2aac69d54c8eb11718ada3d2403
Dmitry R <dimsog@gmail.com>
2021-11-11 14:08:06 +0300

    Add component boilerplate

---
+++ components/Slider.php
@@ -0,0 +1,19 @@
+<?php namespace Dimsog\Slider\Components;
+
+use Cms\Classes\ComponentBase;
+
+class Slider extends ComponentBase
+{
+    public function componentDetails()
+    {
+        return [
+            'name'        => 'Slider Component',
+            'description' => 'No description provided yet...'
+        ];
+    }
+
+    public function defineProperties()
+    {
+        return [];
+    }
+}
