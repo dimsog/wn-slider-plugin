1cd2634b8c24870bf58624e4a011d34899f92df6
Dmitry R <dimsog@gmail.com>
2021-11-12 16:05:08 +0300

    Add swiper

--- components/Slider.php
+++ components/Slider.php
@@ -7,6 +7,12 @@ use Dimsog\Slider\Models\Category;
 
 class Slider extends ComponentBase
 {
+    public function onRun()
+    {
+        $this->controller->addCss('/plugins/dimsog/slider/assets/swiper/swiper.min.css');
+        $this->controller->addJs('/plugins/dimsog/slider/assets/swiper/swiper.min.js');
+    }
+
     public function componentDetails()
     {
         return [
