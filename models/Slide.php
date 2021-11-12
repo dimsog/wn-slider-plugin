69e66f3a270c215f6151783654accf85be85a56c
Dmitry R <dimsog@gmail.com>
2021-11-12 15:54:16 +0300

    Add phpdoc

--- models/Slide.php
+++ models/Slide.php
@@ -6,6 +6,19 @@ use Model;
 use System\Models\File;
 use Winter\Storm\Database\Traits\Sortable;
 
+/**
+ * @property int $id
+ * @property int $category_id
+ * @property Category $category
+ * @property File $image
+ * @property string|null $name
+ * @property string|null $sub_name
+ * @property string|null $link
+ * @property string|null $button_text
+ * @property string|null $text
+ * @property int $position
+ * @property int $active
+ */
 class Slide extends Model
 {
     use \Winter\Storm\Database\Traits\Validation;
